77c043c1686c3c618e1a233794a45c67ba253ae4
JAIDEEP <korrapatijaideep048@gmail.com>
2025-09-06 12:30:59 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  "projectId": "classzen-xii2b",
+  "appId": "1:715575242272:web:caf1bc94f03c7ebfa59645",
+  "storageBucket": "classzen-xii2b.firebasestorage.app",
+  "apiKey": "AIzaSyDzx0Wr7MArFuc0DrSgpyk63XvFo2MRJr4",
+  "authDomain": "classzen-xii2b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "715575242272"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const auth = getAuth(app);
+
+export { app, auth };
